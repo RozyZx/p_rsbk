815d05a136c6c3a839e2d46ab50cb64719953198
RozyZx <rozyzzx2701@gmail.com>
2020-11-29 15:48:23 +0700

    minimal requirement

--- modul5/src/index.js
+++ modul5/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApiTmdb from './apiTmdb';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApiTmdb />
   </React.StrictMode>,
   document.getElementById('root')
 );
