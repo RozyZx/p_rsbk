f8344fff551e0e98900607ef7d7d0c6f2901dc55
RozyZx <rozyzzx2701@gmail.com>
2020-11-30 22:29:54 +0700

    requirement: modul 1, 2, 5

---
+++ tugasakhir/src/apiCard.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import styled, { css } from 'styled-components';
+
+const CardHolder = styled.div`
+    display: inline-grid;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Card = styled.div`
+    display: inline;
+    width: 195px;
+    box-shadow: 0 0 5px 0 rgba(0,0,0,0.2);
+    &:hover {
+        box-shadow: 0 0 5px 5px rgba(0,0,0,0.2);
+    }
+`
+
+const Poster = styled.img`
+    width: 100%;
+    height: 300px;
+`
+
+const CardBody = styled.div`
+    padding: 1.5rem;
+`
+
+export default class apiCard extends Component {
+    render() {
+        return (
+            <CardHolder>
+                <Card onClick={() => this.props.handleOverview(this.props.title, this.props.overview)}>
+                    <Poster src={(this.props.poster == null) ? '/not_found.png' : ("http://image.tmdb.org/t/p/w185" + this.props.poster)} />
+                    <CardBody>
+                        <h5>{this.props.title}</h5>
+                        <p>{this.props.release_date}</p>
+                    </CardBody>
+                </Card>
+            </CardHolder>
+        )
+    }
+}
--- tugasakhir/src/apiTmdb.js
+++ tugasakhir/src/apiTmdb.js
@@ -2,8 +2,47 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Modal } from "antd";
 import "antd/dist/antd.css";
+import styled, { css } from 'styled-components';
+import ApiCard from './apiCard';
+
+//grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(195px, 1fr));
+    margin: 20px;
+    grid-column-gap: 10px;
+    grid-row-gap: 20px;
+`
+
+// const CardHolder = styled.div`
+//     display: inline-grid;
+//     margin-left: auto;
+//     margin-right: auto;
+// `
+
+// const Card = styled.div`
+//     display: inline;
+//     width: 195px;
+//     box-shadow: 0 0 5px 0 rgba(0,0,0,0.2);
+//     &:hover {
+//         box-shadow: 0 0 5px 5px rgba(0,0,0,0.2);
+//     }
+// `
+
+// const Poster = styled.img`
+//     width: 100%;
+//     height: 300px;
+// `
+
+// const CardBody = styled.div`
+//     padding: 1.5rem;
+// `
 
 export default class apiTmdb extends Component {
+
+    api_key = '9ad740896cb3ce8aefcf4103e4bdbd0';
+
     constructor(props) {
         super(props);
         this.state = {
@@ -13,25 +52,29 @@ export default class apiTmdb extends Component {
             release_date: '',
             overview: ''
         }
+        var handleOverview = this.handleOverview.bind(this);
     }
 
     handleButton = (overview) => {
         alert(overview);
     }
 
-    handleOverview = (titleText, overviewText) => {
+    handleOverview(titleText, overviewText) {
         this.setState({
-            visible: true,
+            visible: (overviewText != ''),
             title: titleText,
             overview: overviewText
         })
     }
 
-    componentDidMount() {
-        console.log('did mount called')
+    getData(page) {
         axios({
             method: "get",
-            url: "https://api.themoviedb.org/3/discover/movie?api_key=9ad740896cb3ce8aefcf4103e4bdbd08&language=&sort_by=POPULARITY&include_adult=false&include_video=false&page=1&with_original_language=",
+            url: "https://api.themoviedb.org/3/discover/movie?api_key=" +
+                this.api_key +
+                "8&language=&sort_by=POPULARITY&include_adult=false&include_video=false&page=" +
+                page +
+                "&with_original_language=",
             headers: {
                 accept: "*/*",
             },
@@ -47,43 +90,42 @@ export default class apiTmdb extends Component {
             });
     }
 
+    componentDidMount() {
+        this.getData(1)
+    }
+
     render() {
         return (
             <div>
                 <div className="boxWhite">
-                    <center>
-                        <h1>First page TMDB movie list by popularity</h1>
-                    </center>
+                    <div className="navBar">
+                        <img src="/dark_iconFull.png" />
+                        <p className="about">Kelompok 46</p>
+                    </div>
 
                     <Modal
                         title={this.state.title}
                         centered
                         visible={this.state.visible}
-                        onCancel={() => {this.setState({visible: false})}}
+                        onCancel={() => { this.setState({ visible: false }) }}
                         footer={null}>
                         <p>
                             {this.state.overview}
                         </p>
                     </Modal>
 
-                    {this.state.result.map((results, index) => {
-                        return (
-                            <div className="card" key={results.id}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{results.title}</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">
-                                        {results.release_date}
-                                    </h6>
-                                </div>
-                                <button
-                                    className="button"
-                                    onClick={() => this.handleOverview(results.title, results.overview)}
-                                >
-                                    Overview
-                                </button>
-                            </div>
-                        )
-                    })}
+                    <Container>
+                        {this.state.result.map((results, index) => {
+                            return (
+                                <ApiCard 
+                                    poster={results.poster_path}
+                                    title={results.title}
+                                    release_date={results.release_date}
+                                    handleOverview={this.handleOverview.bind(this)}
+                                    overview={results.overview} />
+                            )
+                        })}
+                    </Container>
 
                 </div>
             </div>
