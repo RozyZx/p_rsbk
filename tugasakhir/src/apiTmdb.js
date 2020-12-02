f623b7ad5185fa702ec41bfcf7dfd4c6bd061e18
RozyZx <rozyzzx2701@gmail.com>
2020-12-02 08:47:27 +0700

    responsi thingamajig

--- tugasakhir/src/apiTmdb.js
+++ tugasakhir/src/apiTmdb.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Modal } from "antd";
 import "antd/dist/antd.css";
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import ApiCard from './apiCard';
 
 //grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
@@ -39,7 +39,63 @@ const Container = styled.div`
 //     padding: 1.5rem;
 // `
 
+// ================= challenge section =================
+/*const posts = [
+    {
+      title: 'Post One',
+      body: 'This is post one'
+    },
+    {
+      title: 'Post Two',
+      body: 'This is post two'
+    }
+  ]
+
+  
+const getPosts = () => {
+    setTimeout( () => {
+      posts.map( post => {
+        console.log(post);
+      })
+    }, 1000);
+  }
+  
+// contoh pembuatan promise
+const createPost = (post) => {
+    return new Promise( (resolve,reject) => {
+      setTimeout(() => {
+        posts.push(post);
+        const state = false;
+        (!state) ? resolve() : reject();
+      }, 2000);
+    })
+  }
+
+    
+  // Async-Await
+  const init = async () => {
+    // Berjalan secara Asynchronous
+    await createPost({
+      "title" : "Post Four",
+      "body"  : "This is post Four"
+    });
+    await getPosts();
+    console.log('babon');
+  }*/
+  // ============== end of challenge section ==============
+
 export default class apiTmdb extends Component {
+
+    sortByProperty(property){  
+        return function(a,b){  
+           if(a[property] > b[property])  
+              return 1;  
+           else if(a[property] < b[property])  
+              return -1;  
+       
+           return 0;  
+        }  
+     }
 
     api_key = '9ad740896cb3ce8aefcf4103e4bdbd0';
 
@@ -52,7 +108,6 @@ export default class apiTmdb extends Component {
             release_date: '',
             overview: ''
         }
-        var handleOverview = this.handleOverview.bind(this);
     }
 
     handleButton = (overview) => {
@@ -82,7 +137,7 @@ export default class apiTmdb extends Component {
             .then((data) => {
                 console.log(data.data);
                 this.setState({
-                    result: data.data.results,
+                    result: data.data.results.sort(this.sortByProperty('vote_average')),
                 });
             })
             .catch((error) => {
@@ -91,7 +146,8 @@ export default class apiTmdb extends Component {
     }
 
     componentDidMount() {
-        this.getData(1)
+        this.getData(1);
+        //init(); //challenge
     }
 
     render() {
@@ -117,7 +173,7 @@ export default class apiTmdb extends Component {
                     <Container>
                         {this.state.result.map((results, index) => {
                             return (
-                                <ApiCard 
+                                <ApiCard key={results.id}
                                     poster={results.poster_path}
                                     title={results.title}
                                     release_date={results.release_date}
